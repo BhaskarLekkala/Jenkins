d5494f1643868bb8876e569c240dd0e10c1bfcee
BHASKAR LEKKALA - SD59324 <bhaskar.lekkala@external.stellantis.com>
2024-05-10 13:14:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins and Docker on Udemy 
         </a>
       </header>
       <p>
